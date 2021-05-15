559bea281a2ee4789d472e3df8a7f26e92117736
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-15 11:11:09 +0530

    using axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
+require('dotenv').config();
 
 class App extends React.Component {
 
     onSearchSubmit(term) {
-
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: term
+            },
+            headers: {
+                Authorization: 'Client-ID E2MCD8pzEu0KMBsXqQIhWLOcJv59wEvFXP6LgaxXbrY'
+            }
+        }).then((response) => {
+            console.log(response);
+        });
     }
 
     render() {
