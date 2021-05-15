45883e5fa1938c8e3f8514366f4f83de93754565
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-15 11:26:37 +0530

    axios custom client

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import axios from 'axios';
 import SearchBar from './SearchBar';
+import unsplash from '../api/unsplash';
+
 require('dotenv').config();
 
 class App extends React.Component {
@@ -8,11 +9,8 @@ class App extends React.Component {
     state = { images: [] };
 
     onSearchSubmit = async (term) => {
-        const response = await axios.get('/search/photos', {
-            params: {
-                query: term
-            },
-
+        const response = await unsplash.get('/search/photos', {
+            params: { query: term }
         });
 
         this.setState({ images: response.data.results });
