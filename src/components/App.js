2bbea51081878397866a7bc69e3b268bd2e3f9b6
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-15 10:51:03 +0530

    added axios for APIs

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
     onSearchSubmit(term) {
-        console.log(term);
+
     }
 
     render() {
