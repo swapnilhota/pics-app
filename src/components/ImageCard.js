129af9f21e7c9718a9559b53cae039539e552781
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-16 09:25:46 +0530

    added grid row span

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { spans: 0 };
+
+        this.imageRef = React.createRef();  //ref to reference DOM element in React
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 150);
+
+        this.setState({ spans });
+    }
+
     render() {
 
         const { description, urls } = this.props.image;
 
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img
+                    ref={this.imageRef}
                     alt={description}
                     src={urls.regular}
                 />
